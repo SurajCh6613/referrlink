88ca340c3657e6f1d60ae71afdc85144352405dd
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-07-29 01:19:01 +0530

    userRoute implemented

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const ConnectDB = require("./config/ConnectDB");
+const userRoutes = require("./routes/userRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-ConnectDB()
+ConnectDB();
+
+// Middlewares
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
