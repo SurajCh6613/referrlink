4fc23a222acfbaa436d7b4624935cbeb81772e57
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-07-29 23:36:37 +0530

    userController updated

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ const express = require("express");
 const ConnectDB = require("./config/ConnectDB");
 const userRoutes = require("./routes/userRoutes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 ConnectDB();
 
 // Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
