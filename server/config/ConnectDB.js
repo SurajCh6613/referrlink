ec2c23d3af1bd2de98671371f006cbef75ccdd87
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-08-21 17:25:40 +0530

    db setup

--- server/config/ConnectDB.js
+++ server/config/ConnectDB.js
@@ -3,11 +3,15 @@ const logger = require("../Logger/logger");
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected successfully");
   } catch (error) {
     console.error(`DB Connection error ${error}`);
     logger.error("DB Connection Error");
+    process.exit(1);
   }
 };
 
